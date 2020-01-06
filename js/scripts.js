abf35e1e40ca923e25c21d9ec82a038ef39e2212
Eric Marcanio <ericmarcanio@gmail.com>
2020-01-06 14:57:58 -0600

    Better visuals

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,48 @@ function typeWriter() {
     }
     setTimeout(typeWriter, speed);
 }
+
+//Scrolling feature down the page
+$(document).ready(function() {
+    //Click and scroll functions
+    jQuery('a').click(function() {
+        jQuery('html, body').animate({
+            scrollTop: jQuery(jQuery(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    //Objects appear when they show up on the screen
+    $(window).scroll(function() {
+
+        /* Check the location of each desired element */
+        $('.hideme').each(function(i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+
+            }
+
+        });
+
+    });
+
+    //moduals for projects
+    $(".modal-trigger").click(function(e) {
+        e.preventDefault();
+        dataModal = $(this).attr("data-modal");
+        $('#' + dataModal).css({ "display": "block" });
+        // $("body").css({"overflow-y": "hidden"}); //Prevent double scrollbar.
+    });
+
+    $(".close-modal, .modal-sandbox").click(function() {
+        $(".modal").css({ "display": "none" });
+        // $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
+    });
+
+});
