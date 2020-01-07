d076dd108609ab4674ca9e6a532cc7188e630241
Eric Marcanio <ericmarcanio@gmail.com>
2020-01-06 18:02:25 -0600

    Lists, Scroll animation

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,8 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
 }
 
+
+
 //Scrolling feature down the page
 $(document).ready(function() {
     //Click and scroll functions
@@ -65,5 +67,32 @@ $(document).ready(function() {
         $(".modal").css({ "display": "none" });
         // $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
     });
+
+    // Create timeline
+    $('#experience-timeline').each(function() {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function() {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function() {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function() {
+            var date = $(this).data('date');
+            if (date) { // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
+            }
+        });
+
+    });
+
 
 });
